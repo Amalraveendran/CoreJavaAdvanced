10abd44cead99cca34f43cc41aa06a33ad3cd222
Silpa <shilpa95mani@gmail.com>
2026-06-14 22:45:19 +0530

    commits for chatper 2 by silpa

---
+++ CoreJava/src/com/study/chaptertwo/VariablesPrimitives.java
@@ -0,0 +1,26 @@
+package com.study.chaptertwo;
+
+public class VariablesPrimitives {
+    public static void main(String[] args) {
+        //Q1
+        String breed = "Dino Breed";
+        double height = 23.6;
+        double length = 40;
+        double weight = 53.45;
+        System.out.println("The " + breed + " is " + height + " inches tall, "
+                + length + " inches long and weighs " + weight + " pounds.");
+
+        //Q2
+        int age = 15;
+        String name = "Dino";
+        char carnivoreCheck = 'Y';
+        System.out.println(name + " is " + age + " years old and is a carnivore with checked value : " + carnivoreCheck);
+
+        //Q3
+        long maxNum = 5000;
+        System.out.println("The maximum number of people allowed in the park is " + maxNum);
+
+        //Q4
+
+    }
+}
