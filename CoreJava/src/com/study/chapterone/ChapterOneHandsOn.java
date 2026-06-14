5fc45b68b4c25fdabf04b14b1b1e3019cd35f35b
Silpa <shilpa95mani@gmail.com>
2026-06-14 12:47:26 +0530

    changes

---
+++ CoreJava/src/com/study/chapterone/ChapterOneHandsOn.java
@@ -0,0 +1,14 @@
+package com.study.chapterone;
+
+public class ChapterOneHandsOn {
+    public static void main(String[] args){
+        String name = "Silpa ";
+        String position = "Manager";
+        String opening_hours = "9:00 AM to 5:00 PM";
+        String closing_hours = "5:00 PM to 9:00 PM";
+        System.out.println("Welcome to Mesozoic Eden");
+        System.out.println("My name is " + name + "and I want to be a "+ position +
+                        " in Mesozoic Eden.");
+        System.out.println("The opening hours of park is from " + opening_hours + " and closing hours are from " + closing_hours);
+    }
+}
